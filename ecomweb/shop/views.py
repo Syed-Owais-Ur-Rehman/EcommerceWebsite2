4aee768ceb360615fa7ce56b051e387c0c2e5d9f
Syed Owais Ur Rehman <52346980+OviLaka@users.noreply.github.com>
2020-07-06 01:16:32 +0500

    Update views.py

--- ecomweb/shop/views.py
+++ ecomweb/shop/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+# from .models import --model--
+
+
+# For Product objects to show use the following code
+# ctrl+shift+p > Preferences: Configure Language Specific Settings > Python
+# "python.linting.pylintArgs": [
+#         "--load-plugins=pylint_django",
+#     ]
+
 
 # Create your views here.
+from django.http import HttpResponse
+
+def index(resquest):
+    return render(request, 'shop/index.html')
+
+def contact(request):
+    return HttpResponse('we are Contact')
+
+def login(request):
+    return HttpResponse('we are Login')
+
+def checkout(request):
+    return HttpResponse('we are Checkout')
+
+def search(request):
+    return HttpResponse('we are Search')
+
+def tracker(request):
+    return HttpResponse('we are Tracker')
+
+def productview(request):
+    return HttpResponse('we are Product View')
