b252b17bd37660fad8874e2dfe62d274998f174c
Syed Owais Ur Rehman <52346980+OviLaka@users.noreply.github.com>
2020-07-06 01:16:35 +0500

    Create urls.py

---
+++ ecomweb/shop/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.index, name = "ShopHome"),
+    path("contact/", views.contact, name = "ContactUs"),
+    path("login/", views.login, name = "Login"),
+    path("checkout/", views.checkout, name = "Checkout"),
+    path("search", views.search, name = "Search"),
+    path("tracker/", views.tracker, name = "TrackingStatus"),
+    path("products/", views.productview, name = "ProductView"),
+
+
+
+]
