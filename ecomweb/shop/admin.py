431e089f2bfaf58d2d548cfcd5ae4311564ab953
Syed Owais Ur Rehman <52346980+OviLaka@users.noreply.github.com>
2020-08-11 18:12:03 +0500

    Update admin.py

--- ecomweb/shop/admin.py
+++ ecomweb/shop/admin.py
@@ -2,3 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 # from .models import --model--
+from .models import Product
+# ,Userid
+
+admin.site.register(Product)
+# admin.site.register(Userid)
