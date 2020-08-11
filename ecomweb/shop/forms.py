1a2fb88acee272ede2ca18ca8ef217e6c9767d31
Syed Owais Ur Rehman <52346980+OviLaka@users.noreply.github.com>
2020-08-11 18:12:06 +0500

    Create forms.py

---
+++ ecomweb/shop/forms.py
@@ -0,0 +1,18 @@
+# from django.forms import ModelForm
+# from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.models import User
+# from django import forms
+
+# from .models import *
+
+# class UploadFileForm(forms.Form):
+#     title = forms.CharField(max_length=50)
+#     file = forms.FileField()
+#     image = models.ImageField(upload_to="shop/image", default="")
+
+# from django import forms
+
+# class DocumentForm(forms.Form):
+#     image = forms.FileField(
+#         label='Select a file',
+#     )
