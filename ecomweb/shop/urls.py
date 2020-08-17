5b2466533ca3cfb1f3dfdf78c840024cb22f5909
Syed Owais Ur Rehman <52346980+OviLaka@users.noreply.github.com>
2020-08-18 00:10:44 +0500

    Update urls.py

--- ecomweb/shop/urls.py
+++ ecomweb/shop/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name = "ShopHome"),
-    path("admin/", views.admin, name = "AdminPage"),
+    path("dashboard/", views.dashboard, name = "Dashboard"),
     path("addform/", views.addform, name = "AddForm"),
     path("login/", views.login, name = "Login"),
     path("signup/", views.signup, name = "SignUp"),
