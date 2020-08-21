c2d2b3d4f659924782320c9adc68ccb253742eb4
Syed Owais Ur Rehman <52346980+OviLaka@users.noreply.github.com>
2020-08-21 07:23:41 +0500

    Update views.py

--- ecomweb/shop/views.py
+++ ecomweb/shop/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, HttpResponse, redirect
 from .models import Product, Customer
 from django.contrib import messages
-from django.contrib.auth import authenticate, login as dj_login, logout
+from django.contrib.auth import authenticate, login as dj_login, logout as dj_logout
 from math import ceil
 from datetime import date
 from django.contrib.auth.models import User
@@ -38,31 +38,40 @@ def index(request):
     return render(request, 'shop/index.html', params)
 
 def dashboard(request):
-    allprods = []
-    catprods = Product.objects.values('owner', 'id')
-    cats = {item['owner'] for item in catprods}
-    for cat in cats:
-        prod = Product.objects.filter(owner = cat)
-        n = len(prod)
-        nslides = n//4 + ceil((n/4)-(n//4))
-        allprods.append([prod, range(1, nslides), nslides])
+    if request.user.is_authenticated:
+        allprods = []
+        catprods = Product.objects.values('owner', 'id')
+        cats = {item['owner'] for item in catprods}
+        for cat in cats:
+            prod = Product.objects.filter(owner = cat)
+            n = len(prod)
+            nslides = n//4 + ceil((n/4)-(n//4))
+            allprods.append([prod, range(1, nslides), nslides])
 
-    params = {'allprods' : allprods}
+        params = {'allprods' : allprods}
+    
+    else:
+        return redirect("/shop/error")
+
     return render(request, 'shop/dashboard.html',params)
 
 def addform(request):
-    if request.method=="POST":
-        print(request)
-        name = request.POST.get('name', '')
-        owner = request.POST.get('loggedinuser')
-        category = request.POST.get('category', '')
-        email = request.POST.get('email', '')
-        price = request.POST.get('price', '')
-        desc = request.POST.get('desc', '')
-        image = request.FILES['image']
-        pub_date = request.POST.get('date', '')
-        addform = Product(product_name=name, owner=owner, email=email, price=price, desc=desc, image=image, category=category, pub_date=pub_date)
-        addform.save()
+    if request.user.is_authenticated:
+        if request.method=="POST":
+            print(request)
+            name = request.POST.get('name', '')
+            owner = request.POST.get('loggedinuser')
+            category = request.POST.get('category', '')
+            email = request.POST.get('email', '')
+            price = request.POST.get('price', '')
+            desc = request.POST.get('desc', '')
+            image = request.FILES['image']
+            pub_date = request.POST.get('date', '')
+            addform = Product(product_name=name, owner=owner, email=email, price=price, desc=desc, image=image, category=category, pub_date=pub_date)
+            addform.save()
+            return redirect("/shop/dashboard/")
+    else:
+        return redirect("/shop/error")
     return render(request, 'shop/addform.html')
 
 def login(request):
@@ -84,9 +93,9 @@ def login(request):
     return render(request, 'shop/login.html',context)
 
 def logout(request):
-    logout(request)
+    dj_logout(request)
     messages.success(request, "Successfully Logged Out")
-    return redirect("shop/index.html")
+    return render(request, 'shop/logout.html')
 
 def signup(request):
     if request.method=="POST":
@@ -140,80 +149,63 @@ stripe.api_key = settings.STRIPE_SECRET_KEY
 
 def checkout(request):
     publishkey = settings.STRIPE_PUBLISHABLE_KEY
-    if request.method == 'POST':
-        token = request.POST.get('stripeToken')
-        print (token)
-        print('Data:', request.POST)
-        order_email = request.POST.get('customeremail')
-        fname = request.POST.get('fname')
-        lname = request.POST.get('lname')
-        add1 = request.POST.get('add1')
-        add2 = request.POST.get('add2')
-        country = request.POST.get('country')
-        city = request.POST.get('city')
-        phoneno = request.POST.get('phone')
-        orderdate = date.today()
-        totalprice = request.POST.get('totalprice')
-        customer_amount = request.POST.get('amount')
-        orderform = Customer(fname=fname, lname=lname, address1=add1, address2= add2, customer_email=order_email, country=country, city=city, phone=phoneno, totalprice=totalprice, order_date=orderdate)
-        orderform.save()
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            token = request.POST.get('stripeToken')
+            print (token)
+            print('Data:', request.POST)
+            order_email = request.POST.get('customeremail')
+            fname = request.POST.get('fname')
+            lname = request.POST.get('lname')
+            add1 = request.POST.get('add1')
+            add2 = request.POST.get('add2')
+            country = request.POST.get('country')
+            city = request.POST.get('city')
+            phoneno = request.POST.get('phone')
+            orderdate = date.today()
+            totalprice = request.POST.get('totalprice')
+            customer_amount = request.POST.get('amount')
+            orderform = Customer(fname=fname, lname=lname, address1=add1, address2= add2, customer_email=order_email, country=country, city=city, phone=phoneno, totalprice=totalprice, order_date=orderdate)
+            orderform.save()
 
-        try:   
-            customer = stripe.Customer.create(
-                email=order_email,
-                name=fname + ' ' + lname,
-                balance=customer_amount
-                )
-            
-            payment = stripe.PaymentIntent.create(
-                customer=customer,
-                amount=customer_amount,
-                # (totalprice*100)
-                currency='usd',
-                payment_method_types=['card'],
-                payment_method='pm_card_visa',
-                source=token,
-                receipt_email= order_email,
-                description='Customer Purchase'
-                )
-            # charge = stripe.Charge.create(
-            #     customer=customer,
-            #     amount=customer_amount,
-            #     currency="usd",
-            #     source=token,
-            #     description="Example Charge"
-            # )
-        except stripe.error.CardError as e:
-        # The card has been declined
-         messages.info(request, "Your Card Has Been Declined")
+            try:   
+                customer = stripe.Customer.create(
+                    email=order_email,
+                    name=fname + ' ' + lname,
+                    balance=customer_amount
+                    )
+                
+                payment = stripe.PaymentIntent.create(
+                    customer=customer,
+                    amount=customer_amount,
+                    # (totalprice*100)
+                    currency='usd',
+                    payment_method_types=['card'],
+                    payment_method='pm_card_visa',
+                    source=token,
+                    receipt_email= order_email,
+                    description='Customer Purchase'
+                    )
+                # charge = stripe.Charge.create(
+                #     customer=customer,
+                #     amount=customer_amount,
+                #     currency="usd",
+                #     source=token,
+                #     description="Example Charge"
+                # )
+            except stripe.error.CardError as e:
+            # The card has been declined
+                messages.info(request, "Your Card Has Been Declined")
+    else:
+        return redirect("/shop/error")    
     context = {'publishkey':publishkey}
     return render(request, 'shop/checkout.html',context)
 
-# def ch(request):
-#     publishkey = settings.STRIPE_PUBLISHABLE_KEY
-#     if request.method == 'POST':
-#         token = request.POST['stripeToken']
-#         print (token)
-#         # # Create a charge: this will charge the user's card
-#         # try:
-#         #     charge = stripe.Charge.create(
-#         #         amount=1200,
-#         #         currency="usd",
-#         #         source=token,
-#         #         description="Example Charge"
-#         #     )
-#         # except strip.error.CardError as e:
-#         #     # The card has been declined
-#         #     message.info(request, "Your Card Has Been Declined")
-#     context = {'publishkey':publishkey}
-#     return render(request, 'shop/ch.html')
+def error(request):
+    return render(request, 'shop/error.html')
 
-def search(request):
-    return HttpResponse('we are Search')
+def productview(request, myid):
+    product = Product.objects.filter(id=myid)
 
-def tracker(request):
-    return HttpResponse('we are Tracker')
-
-def productview(request):
-    return HttpResponse('we are Product View')
+    return render(request, 'shop/productview.html', {'product':product[0]})
     
