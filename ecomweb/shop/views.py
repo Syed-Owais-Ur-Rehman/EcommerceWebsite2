0a2bf682b5315d3699e41df9715f008d56b14f2c
Syed Owais Ur Rehman <52346980+OviLaka@users.noreply.github.com>
2020-08-22 01:58:10 +0500

    Update views.py

--- ecomweb/shop/views.py
+++ ecomweb/shop/views.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+import stripe
 from django.shortcuts import render, HttpResponse, redirect
 from .models import Product, Customer
 from django.contrib import messages
@@ -15,27 +17,28 @@ from django.contrib.auth.models import User
 
 
 # Create your views here.
-# from django.http import 
+# from django.http import
 
 def index(request):
     #products = Product.objects.all()
-    #print(products)
+    # print(products)
     #n = len(products)
-    
+
     # params = {'no_of_slides': nslides, 'range': range(nslides), 'product': products}
-    #allprods = [[products, range(1,nslides), nslides],
+    # allprods = [[products, range(1,nslides), nslides],
     #            [products, range(1,nslides), nslides]]
     allprods = []
     catprods = Product.objects.values('category', 'id')
     cats = {item['category'] for item in catprods}
     for cat in cats:
-        prod = Product.objects.filter(category = cat)
+        prod = Product.objects.filter(category=cat)
         n = len(prod)
         nslides = n//4 + ceil((n/4)-(n//4))
         allprods.append([prod, range(1, nslides), nslides])
 
-    params = {'allprods' : allprods}
+    params = {'allprods': allprods}
     return render(request, 'shop/index.html', params)
+
 
 def dashboard(request):
     if request.user.is_authenticated:
@@ -43,21 +46,22 @@ def dashboard(request):
         catprods = Product.objects.values('owner', 'id')
         cats = {item['owner'] for item in catprods}
         for cat in cats:
-            prod = Product.objects.filter(owner = cat)
+            prod = Product.objects.filter(owner=cat)
             n = len(prod)
             nslides = n//4 + ceil((n/4)-(n//4))
             allprods.append([prod, range(1, nslides), nslides])
 
-        params = {'allprods' : allprods}
-    
+        params = {'allprods': allprods}
+
     else:
         return redirect("/shop/error")
 
-    return render(request, 'shop/dashboard.html',params)
+    return render(request, 'shop/dashboard.html', params)
+
 
 def addform(request):
     if request.user.is_authenticated:
-        if request.method=="POST":
+        if request.method == "POST":
             print(request)
             name = request.POST.get('name', '')
             owner = request.POST.get('loggedinuser')
@@ -67,16 +71,18 @@ def addform(request):
             desc = request.POST.get('desc', '')
             image = request.FILES['image']
             pub_date = request.POST.get('date', '')
-            addform = Product(product_name=name, owner=owner, email=email, price=price, desc=desc, image=image, category=category, pub_date=pub_date)
+            addform = Product(product_name=name, owner=owner, email=email, price=price,
+                              desc=desc, image=image, category=category, pub_date=pub_date)
             addform.save()
             return redirect("/shop/dashboard/")
     else:
         return redirect("/shop/error")
     return render(request, 'shop/addform.html')
 
+
 def login(request):
     context = {}
-    if request.method=="POST":
+    if request.method == "POST":
         # Get the post parameters
         loginusername = request.POST['username']
         loginpassword = request.POST['password']
@@ -90,15 +96,17 @@ def login(request):
             messages.error(request, "Invalid Credentials, Please Try Again")
             return redirect("/shop/login")
 
-    return render(request, 'shop/login.html',context)
+    return render(request, 'shop/login.html', context)
+
 
 def logout(request):
     dj_logout(request)
     messages.success(request, "Successfully Logged Out")
     return render(request, 'shop/logout.html')
 
+
 def signup(request):
-    if request.method=="POST":
+    if request.method == "POST":
         # Get the post parameters
         username = request.POST['username']
         fname = request.POST['fname']
@@ -117,42 +125,41 @@ def signup(request):
     # else:
     #     return HttpResponse("404 - Not Found")
 
-    #check errors
+    # check errors
 
-    return render(request, 'shop/signup.html') 
+    return render(request, 'shop/signup.html')
+
 
 def success(request):
     context = {}
     context['user'] = request.user
     messages.success(request, "Your Account Has Been Successfully Created")
-    return render(request, 'shop/success.html', context) 
+    return render(request, 'shop/success.html', context)
+check = 0
 
 def cart(request):
     allprods = []
     catprods = Product.objects.values('category', 'id')
     cats = {item['category'] for item in catprods}
     for cat in cats:
-        prod = Product.objects.filter(category = cat)
+        prod = Product.objects.filter(category=cat)
         n = len(prod)
         nslides = n//4 + ceil((n/4)-(n//4))
         allprods.append([prod, range(1, nslides), nslides])
 
-    params = {'allprods' : allprods}
+    params = {'allprods': allprods}
     return render(request, 'shop/cart.html', params)
 
 
-
-import stripe
-from django.conf import settings
-
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
+
 def checkout(request):
+    params = {}
     publishkey = settings.STRIPE_PUBLISHABLE_KEY
-    if request.user.is_authenticated:
-        if request.method == 'POST':
+    if request.method == 'POST':
             token = request.POST.get('stripeToken')
-            print (token)
+            print(token)
             print('Data:', request.POST)
             order_email = request.POST.get('customeremail')
             fname = request.POST.get('fname')
@@ -165,16 +172,17 @@ def checkout(request):
             orderdate = date.today()
             totalprice = request.POST.get('totalprice')
             customer_amount = request.POST.get('amount')
-            orderform = Customer(fname=fname, lname=lname, address1=add1, address2= add2, customer_email=order_email, country=country, city=city, phone=phoneno, totalprice=totalprice, order_date=orderdate)
+            orderform = Customer(fname=fname, lname=lname, address1=add1, address2=add2, customer_email=order_email,
+                                 country=country, city=city, phone=phoneno, totalprice=totalprice, order_date=orderdate)
             orderform.save()
 
-            try:   
+            try:
                 customer = stripe.Customer.create(
                     email=order_email,
                     name=fname + ' ' + lname,
                     balance=customer_amount
-                    )
-                
+                )
+
                 payment = stripe.PaymentIntent.create(
                     customer=customer,
                     amount=customer_amount,
@@ -183,9 +191,9 @@ def checkout(request):
                     payment_method_types=['card'],
                     payment_method='pm_card_visa',
                     source=token,
-                    receipt_email= order_email,
+                    receipt_email=order_email,
                     description='Customer Purchase'
-                    )
+                )
                 # charge = stripe.Charge.create(
                 #     customer=customer,
                 #     amount=customer_amount,
@@ -193,19 +201,26 @@ def checkout(request):
                 #     source=token,
                 #     description="Example Charge"
                 # )
+                return redirect("/shop/transaction/")
+
             except stripe.error.CardError as e:
-            # The card has been declined
+                # The card has been declined
                 messages.info(request, "Your Card Has Been Declined")
-    else:
-        return redirect("/shop/error")    
-    context = {'publishkey':publishkey}
-    return render(request, 'shop/checkout.html',context)
+
+    
+    # return redirect("/shop/error")
+    context = {'publishkey': publishkey}
+    return render(request, 'shop/checkout.html', context)
+
 
 def error(request):
     return render(request, 'shop/error.html')
 
+def transaction(request):
+    return render(request, 'shop/transaction.html')
+
+
 def productview(request, myid):
     product = Product.objects.filter(id=myid)
 
-    return render(request, 'shop/productview.html', {'product':product[0]})
-    
+    return render(request, 'shop/productview.html', {'product': product[0]})
